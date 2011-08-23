8ecb693888a60117f59aaf301e48db5328aa770b
Sridhar Ratnakumar <sridharr@activestate.com>
2011-08-22 20:17:04 -0700

    url query building functions
    from http://p.hagelb.org/http-client-send-body

--- src/notaskinnerbox/stackexchange.clj
+++ src/notaskinnerbox/stackexchange.clj
@@ -1,8 +1,9 @@
 (ns notaskinnerbox.stackexchange
-  (:use [clojure.data.json :only (read-json)]
+  (:use [clojure.string :only (join split)]
+        [clojure.data.json :only (read-json)]
         [clojure.contrib.io :only (to-byte-array)])
   (:import [java.util.zip GZIPInputStream]
-           [java.net URL]
+           [java.net URL URLEncoder]
            [java.util Date]))
 
 
@@ -16,18 +17,29 @@
   (* n 1000 60 60 24))
 
 
+(defn- url-encode
+  "Wrapper around java.net.URLEncoder returning a (UTF-8) URL encoded
+representation of text."
+  [text]
+  (URLEncoder/encode text "UTF-8"))
+
+
+(defn- encode-body-map
+  "Turns a map into a URL-encoded string suitable for a request body."
+  [body]
+  (join "&" (map #(join "=" (map url-encode %)) body)))
+
+
 (defn top-posts-url
   [site tag n]
   (let [fromdate
         (if (> n 0)
           (long (/ (- (now) (n-days-timestamp n)) 1000))
           "")]
-    (str "http://api."
-         site
-         "/1.1/questions?fromdate="
-         fromdate
-         "&sort=votes&tagged="
-         tag)))
+    (str "http://api." site "/1.1/questions??"
+         (encode-body-map {"fromdate" (str fromdate)
+                           "sort" "votes"
+                           "tagged" (str tag)}))))
 
 
 (defn curl-gzip
