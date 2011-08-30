c03651fd848eb8c85782987899af440959805329
Sridhar Ratnakumar <sridharr@activestate.com>
2011-08-29 21:34:17 -0700

    hiccup is not needed

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
                  [org.clojure/tools.cli "0.1.0"]
                  [org.clojure/data.json "0.1.1"]
                  [compojure "0.6.4"]
-                 [hiccup "0.3.6"]
                  [enlive "1.0.0"]
                  [ring "0.3.11"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
