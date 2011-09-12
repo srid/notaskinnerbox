e8a3ec0e651f696a8878cc4b9c599a3294ae3e60
Sridhar Ratnakumar <sridharr@activestate.com>
2011-09-11 20:54:26 -0700

    comment out code and get a minimal app running using dev_appserver.sh
    i was seeing appengine specific errors with the app as it is. so wanted
    to get a minimal app running before I can incrementally fix the issues.

--- src/notaskinnerbox/routes.clj
+++ src/notaskinnerbox/routes.clj
@@ -1,36 +1,39 @@
 (ns notaskinnerbox.routes
   (:use compojure.core
         notaskinnerbox.views
-        ring.adapter.jetty
-        ring.middleware.stacktrace
+        ;; ring.adapter.jetty
+        ;; ring.middleware.stacktrace
         [hiccup.middleware :only (wrap-base-url)])
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]
+            ; [compojure.handler :as handler]
             [compojure.response :as response]
             [notaskinnerbox.stackexchange :as sx]))
 
 
-(defroutes main-routes
+(defroutes app-handler
+  (GET "/" [] "hello")
+  (route/resources "/")
+  (route/not-found "Page not found"))
+  
+(comment
   (GET "/" [] (view-digest "stackoverflow.com" "" 7))
   (GET ["/se/:site", :site #"[a-z\.]+"] [site]
        (view-digest site "" 7))
   (GET ["/se/:site/:n", :site #"[a-z\.]+"] [site tag n]
        (view-digest site "" (. Integer parseInt n)))
   (GET ["/se/:site/:n/:tag", :site #"[a-z\.]+"] [site tag n]
-       (view-digest site tag (. Integer parseInt n)))
-
-  (route/resources "/")
-  (route/not-found "Page not found"))
+       (view-digest site tag (. Integer parseInt n))))
 
 
-(def app-handler
+
+(comment def app-handler
   (-> (handler/site main-routes)
-      (wrap-stacktrace)
+      ;(wrap-stacktrace)
       (wrap-base-url)))
 
 
 ;; for heroku/stackato
-(defn -main []
+(comment defn -main []
   (let [port (Integer/parseInt
               (get (System/getenv) "VCAP_APP_PORT"
                    (get (System/getenv) "PORT" "8080")))]
