d7de1d4fba7981f70752269edb665ecd03ab94d3
Sridhar Ratnakumar <sridharr@activestate.com>
2011-09-11 22:24:40 -0700

    refactor running-on-google? forms

--- src/notaskinnerbox/utils.clj
+++ src/notaskinnerbox/utils.clj
@@ -4,6 +4,4 @@
 (defn running-on-google? []
   "Return True if we are running on Google's production servers."
   (let [gae-env (System/getProperty "com.google.appengine.runtime.environment")]
-    (println "runtime: " gae-env)
-    (and gae-env
-         (= gae-env "Production"))))
+    (and (= gae-env "Production"))))
